af82eb89d5838d4947163f385d973d4f576879aa
magnamancer <91973108+magnamancer@users.noreply.github.com>
2025-06-15 15:23:41 -0400

    Admin Update
    Did stuff with admin page

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -52,13 +52,15 @@ class Book(models.Model):
     # Author as a string rather than object because it hasn't been declared yet in file.
 
     summary = models.TextField(
-        max_length=1000, help_text="Enter a brief description of the book"
+        max_length=1000,
+        null=True,
+        help_text="Enter a brief description of the book",
     )
     isbn = models.CharField(
         "ISBN",
         max_length=13,
         unique=True,
-        null=True
+        null=True,
         help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn'
         '">ISBN number</a>',
     )
@@ -76,6 +78,12 @@ class Book(models.Model):
         """Returns the URL to access a detail record for this book."""
         return reverse("book-detail", args=[str(self.id)])
 
+    def display_genre(self):
+        """Create a string for the Genre. This is required to display genre in Admin."""
+        return ", ".join(genre.name for genre in self.genre.all()[:3])
+
+    display_genre.short_description = "Genre"
+
 
 class BookInstance(models.Model):
     """Model representing a specific copy of a book (i.e. that can be borrowed from the library)."""
@@ -88,6 +96,9 @@ class BookInstance(models.Model):
     book = models.ForeignKey("Book", on_delete=models.RESTRICT, null=True)
     imprint = models.CharField(max_length=200)
     due_back = models.DateField(null=True, blank=True)
+    language = models.ForeignKey(
+        "Language", on_delete=models.RESTRICT, null=True
+    )
 
     LOAN_STATUS = (
         ("m", "Maintenance"),
