6ec007028bdba943dfe157df787058af4cd650fa
magnamancer <91973108+magnamancer@users.noreply.github.com>
2025-06-16 12:15:15 -0400

    Finished Locallibrary content

---
+++ locallibrary/catalog/boot_detail.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 16 11:34:13 2025
+
+@author: frc00
+"""
--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -1,4 +1,16 @@
 from django.urls import path
 from . import views
 
-urlpatterns = []
+urlpatterns = [
+    path("", views.index, name="index"),
+    # Book Pages
+    path("books/", views.BookListView.as_view(), name="books"),
+    path("book/<int:pk>", views.BookDetailView.as_view(), name="book-detail"),
+    # Author Pages
+    path("authors/", views.AuthorListView.as_view(), name="authors"),
+    path(
+        "authors/<int:pk>",
+        views.AuthorDetailView.as_view(),
+        name="author-detail",
+    ),
+]
